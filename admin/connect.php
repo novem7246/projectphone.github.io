2e0d55dfe57e0f4844b80bc3f7ad0f9d7d7a3136
novem7246 <110543536+novem7246@users.noreply.github.com>
2022-08-29 01:25:35 +0700

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "12345678", "pongsakorn");
+$conn = mysqli_connect("localhost", "root", "12345678", "project");
 
     if (!$conn) {
         die("Failed to connect to database " . mysqli_error());
